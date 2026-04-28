a53419a27b5d563991bda5f8be27c91c452c0877
Laxmi Hegde Nagaraj <111542177+Laxmihe@users.noreply.github.com>
2026-04-28 12:30:16 +0300

    Change export syntax to CommonJS in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
@@ -9,4 +9,5 @@ export default {
   },
   plugins: [],
 }
+
 
